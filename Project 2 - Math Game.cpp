e64396b18d2848b4dfe9bd67be57dec0f1bb3f68
ZAID MUHAIDAT <zsameh667@gmail.com>
2026-02-26 17:38:50 +0300

    Add files via upload
    Add Project 1 (Stone-Paper-Scissors) and Project 2 (Math Game) to the collection

---
+++ Project 2 - Math Game.cpp
@@ -0,0 +1,137 @@
+﻿#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+enum enOperation { ADDITION = 1, SUBTRACTION = 2, MULTIPLICATION = 3, DIVISION = 4, Mix = 5 };
+enum enDifficultylevel { EASY = 1, MEDIUM = 2, HARD = 3, MixLevel = 4 };
+
+int RandNumber(int From, int To)
+{
+    return rand() % (To - From + 1) + From;
+}
+
+enDifficultylevel ReadDifficultyLevel()
+{
+    short Choicelevel;
+    do
+    {
+        cout << "Enter Difficulty Level: [1] EASY, [2] MEDIUM, [3] HARD, [4] Mix ? ";
+        cin >> Choicelevel;
+    } while (Choicelevel < 1 || Choicelevel > 4);
+
+    return (enDifficultylevel)Choicelevel;
+}
+
+enOperation ReadOperationChoice()
+{
+    short ChoiceType;
+    do
+    {
+        cout << "Enter Operation: [1] ADD, [2] SUB, [3] MUL, [4] DIV, [5] Mix ? ";
+        cin >> ChoiceType;
+    } while (ChoiceType < 1 || ChoiceType > 5);
+
+    return (enOperation)ChoiceType;
+}
+
+void GetRange(enDifficultylevel& Level, int& From, int& To)
+{
+    if (Level == enDifficultylevel::MixLevel)
+        Level = (enDifficultylevel)RandNumber(1, 3);
+
+    if (Level == enDifficultylevel::EASY) { From = 1; To = 10; }
+    else if (Level == enDifficultylevel::MEDIUM) { From = 10; To = 100; }
+    else { From = 100; To = 1000; }
+}
+
+float RightAnswer(int FirstNumber, int SecondNumber, enOperation OperationType)
+{
+    switch (OperationType)
+    {
+    case enOperation::ADDITION: return FirstNumber + SecondNumber;
+    case enOperation::SUBTRACTION: return FirstNumber - SecondNumber;
+    case enOperation::MULTIPLICATION: return FirstNumber * SecondNumber;
+    case enOperation::DIVISION: return (float)FirstNumber / SecondNumber;
+    default: return 0;
+    }
+}
+
+bool AskOneQuestion(enOperation OpType, enDifficultylevel Level)
+{
+    int From, To, A, B;
+    GetRange(Level, From, To);
+
+    A = RandNumber(From, To);
+    B = RandNumber(From, To);
+
+    if (OpType == enOperation::Mix)
+        OpType = (enOperation)RandNumber(1, 4);
+
+    switch (OpType)
+    {
+    case enOperation::ADDITION: cout << A << " + " << B << " = ? "; break;
+    case enOperation::SUBTRACTION: cout << A << " - " << B << " = ? "; break;
+    case enOperation::MULTIPLICATION: cout << A << " * " << B << " = ? "; break;
+    case enOperation::DIVISION: cout << A << " / " << B << " = ? "; break;
+    }
+
+    float UserAnswer;
+    cin >> UserAnswer;
+
+    float Correct = RightAnswer(A, B, OpType);
+
+    if (UserAnswer == Correct)
+    {
+        cout << "✅ Correct!\n";
+        return true;
+    }
+    else
+    {
+        cout << "❌ Wrong, correct answer = " << Correct << "\n";
+        return false;
+    }
+}
+
+void PlayGame()
+{
+    int Rounds;
+    cout << "How many questions? ";
+    cin >> Rounds;
+
+    enDifficultylevel Level = ReadDifficultyLevel();
+    enOperation OpType = ReadOperationChoice();
+
+    int Right = 0, Wrong = 0;
+
+    for (int i = 1; i <= Rounds; i++)
+    {
+        cout << "\n--- Question " << i << " ---\n";
+        if (AskOneQuestion(OpType, Level))
+            Right++;
+        else
+            Wrong++;
+    }
+
+    cout << "\n========= Game Summary =========\n";
+    cout << "Total Questions: " << Rounds << "\n";
+    cout << "Right Answers: " << Right << "\n";
+    cout << "Wrong Answers: " << Wrong << "\n";
+    cout << "================================\n";
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    char Again;
+    do
+    {
+		system("cls");
+        PlayGame();
+        cout << "Play Again? (Y/N): ";
+        cin >> Again;
+    } while (Again == 'Y' || Again == 'y');
+
+    return 0;
+}
---
+++ Stone-Paper-Scissor game.cpp
@@ -0,0 +1,252 @@
+﻿#include<iostream>
+#include<cmath>
+#include<string>
+#include <cstdlib> 
+
+using namespace std;
+
+enum enChoice { Stone = 1, Paper = 2, Scissor = 3 };
+enum enResult { Win = 1, Lose = 2, Draw = 3 };
+
+int ReadPositiveNumber(string Message) // قراءة رقم موجب يمثل عدد تكرار اللعبة
+{
+	int Number;
+	do
+	{
+		cout << Message;
+		cin >> Number;
+	} while (Number <= 0);
+	return Number;
+}
+void WelcomeMassage()
+{
+	cout << "Welcome to the Stone-Paper-Scissor Game!" << endl;
+	cout << "----------------------------------------" << endl;
+	cout << "\n";
+	
+}
+int IsChoiceValid() // اختيار اللاعب والتاكد من صحة الاختيار
+{
+	
+
+	int Choice;	
+	cout << "Please choose one of the following options: " << endl;
+	cout << "1. Stone" << endl;
+	cout << "2. Paper" << endl;
+	cout << "3. Scissor" << endl;
+	cin >> Choice;
+
+	do
+	{
+		if (Choice > 0 || Choice < 4)
+			return Choice;
+		else
+			cout << "Invalid choice, please choose again: ";
+			cin >> Choice;
+			cout << "\n";
+
+	} while (Choice >= 1 || Choice <= 3);
+
+      return Choice;
+}
+
+int GetComputerChoice()
+{
+	int ComputerChoice = rand() % 3 + 1;
+	return ComputerChoice;
+}
+
+int GetPlayerChoice()
+{
+	int PlayerChpice;
+	PlayerChpice = IsChoiceValid();
+	return PlayerChpice;
+}
+
+void PrintComputer(int ComputerChoice)
+{
+	switch (ComputerChoice)
+	{
+	case enChoice::Stone:
+		cout << "Computer choice is: Stone" << endl;
+		break;
+	case enChoice::Paper:
+		cout << "Computer choice is: Paper" << endl;
+		break;
+	case enChoice::Scissor:
+		cout << "Computer choice is: Scissor" << endl;
+		break;
+	default:
+		cout << "Invalid choice!" << endl;
+		break;
+	}
+}
+void PrintPlayerChoice(int PlayerChoice)
+{
+	switch (PlayerChoice)
+	{
+	case enChoice::Stone:
+		cout << "Your choice is: Stone" << endl;
+		break;
+	case enChoice::Paper:
+		cout << "Your choice is: Paper" << endl;
+		break;
+	case enChoice::Scissor:
+		cout << "Your choice is: Scissor" << endl;
+		break;
+	default:
+		cout << "Invalid choice!" << endl;
+		break;
+	}
+}
+
+int GetWinner( int ComputerChoice, int PlayerChoice )      //قواعد اللعبة
+{
+	
+		switch (PlayerChoice)
+		{
+		case enChoice::Stone:
+			if (ComputerChoice == enChoice::Stone)
+				return enResult::Draw;
+			else if (ComputerChoice == enChoice::Paper)
+				return enResult::Lose;
+			else if (ComputerChoice == enChoice::Scissor)
+				return enResult::Win;
+			break;
+
+		case enChoice::Paper:
+			if (ComputerChoice == enChoice::Paper)
+				return enResult::Draw;
+			else if (ComputerChoice == enChoice::Scissor)
+				return enResult::Lose;
+			else if (ComputerChoice == enChoice::Stone)
+				return enResult::Win;
+			break;
+
+		case enChoice::Scissor:
+			if (ComputerChoice == enChoice::Scissor)
+				return enResult::Draw;
+			else if (ComputerChoice == enChoice::Stone)
+				return enResult::Lose;
+			else if (ComputerChoice == enChoice::Paper)
+				return enResult::Win;
+			break;
+
+		default:
+			cout << "Invalid player choice!" << endl;
+			return -1; // Return an invalid result to indicate an error.
+		}
+	
+	/*switch (PlayerChoice)
+	{
+	case enChoice::Stone:
+		if (ComputerChoice == enChoice::Stone)
+			return enResult::Draw;
+		else if (ComputerChoice == enChoice::Paper)
+			return enResult::Lose;
+		else if (ComputerChoice == enChoice::Scissor)
+			return enResult::Win;
+		break;
+
+	case enChoice::Paper:
+		if (ComputerChoice == enChoice::Paper)
+			return enResult::Draw;
+		else if (ComputerChoice == enChoice::Scissor)
+			return enResult::Lose;
+		else if (ComputerChoice == enChoice::Stone)
+			return enResult::Win;
+		break;
+
+	case enChoice::Scissor:
+		if (ComputerChoice == enChoice::Scissor)
+			return enResult::Draw;
+		else if (ComputerChoice == enChoice::Stone)
+			return enResult::Lose;
+		else if (ComputerChoice == enChoice::Paper)
+			return enResult::Win;
+		break;
+
+	default:
+		cout << "Invalid player choice!" << endl;
+		return -1; // Return an invalid result to indicate an error.
+	}*/
+}
+void PrintResult(int result)
+{
+	switch (result)
+	{
+	case enResult::Win:
+		cout << " You win!" << endl;
+		system("color 2");
+		break;
+	case enResult::Lose:
+		cout << " You lose!" << endl;
+		system("color 4");
+		cout << "\a";
+		break;
+	case enResult::Draw:
+		cout << " It's a draw!" << endl;
+		system("color 6");
+		break;
+	default:
+		cout << "Error determining the result!" << endl;
+		break;
+	}
+}
+
+void ClearScreen()
+{
+	system("cls");
+}
+int GameRepeted()
+{
+	char choice;
+	cout << "Do you want to play again? (y/n): ";
+	cin >> choice;
+	if (choice == 'y' || choice == 'Y')
+	{
+		ClearScreen();
+
+		return 1; // Play again  
+	}
+	else if (choice == 'n' || choice == 'N')
+		return 0; // Exit the game  
+	else
+	{
+		cout << "Invalid input. Please enter 'y' or 'n'." << endl;
+		return GameRepeted(); // Recursively ask again  
+	}
+}
+void loopGameRepeted()
+{
+	do
+	{
+		int Number;
+		WelcomeMassage();
+
+		Number = ReadPositiveNumber("How many times do you want to repeat the game? ");
+		for (int i = 0; i < Number; i++)
+		{
+        int ComputerChoice = GetComputerChoice();
+		int PlayerChoice = GetPlayerChoice();
+
+		PrintPlayerChoice(PlayerChoice);
+		PrintComputer(ComputerChoice);
+
+		int result = GetWinner( ComputerChoice, PlayerChoice);
+
+		PrintResult(result);
+		}
+		
+
+	} while (GameRepeted());
+}
+
+
+int main()
+{
+	srand((unsigned)time(NULL)); // Seed the random number generator.
+	loopGameRepeted();
+	return 0;
+}
+
